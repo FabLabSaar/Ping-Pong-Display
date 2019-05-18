19112c2f1ef0cee8cd4613537296d13817475fe0
florian fromm <flrn.frmm@gmail.com>
2019-05-18 23:58:53 +0200

    Update Top-Tile and Bottom-Tile

--- ping-pong-display/tiles/models/bottom-tile.py
+++ ping-pong-display/tiles/models/bottom-tile.py
@@ -2,18 +2,64 @@ doc = FreeCAD.newDocument("Bottom-Tile")
 tile = doc.addObject("Part::Box","Tile")
 tile.Width = 80.0
 tile.Length = 80.0
-tile.Height = 5.0
+tile.Height = 3.0
 
-rabbet_locations = [(x,y) for x in [0.0,75.0] for y in [35.0]]
-for (i,(x,y)) in enumerate(rabbet_locations):
-  r = doc.addObject("Part::Box","Rabbet{0}".format(i+1))
-  r.Width = 10.0
-  r.Length = 5.0
-  r.Height = 3.0
-  r.Placement = App.Placement(App.Vector(x,y,5), App.Rotation(App.Vector(0,0,1),0), App.Vector(0,0,0))
-  # c = doc.addObject("Part::Cut","CutR{0}".format(i+1))
-  # c.Base = lastCut
-  # c.Tool = r
-  # lastCut = c
+chamber_locations = [(x,y) for x in [20.0 - 7.0, 60.0 - 7.0] for y in [3.5]]
+for (i,(x,y)) in enumerate(chamber_locations):
+  ch = doc.addObject("Part::Box","Chamber{0}".format(i+1))
+  ch.Width = 73.0
+  ch.Length = 14.0
+  ch.Height = 2.0
+  ch.Placement.Base = (x,y,1.0)
+  c = doc.addObject("Part::Cut","CutCh{0}".format(i+1))
+  c.Base = tile
+  c.Tool = ch
+  tile = c
+
+floor_locations = [(x,y) for x in [20.0 - 6.0, 60.0 - 6.0] for y in [0.0]]
+for (i,(x,y)) in enumerate(floor_locations):
+  f = doc.addObject("Part::Box","Floor{0}".format(i+1))
+  f.Width = 80.0
+  f.Length = 12.0
+  f.Height = 2.0
+  f.Placement.Base = (x,y,1.0)
+  c = doc.addObject("Part::Cut","CutF{0}".format(i+1))
+  c.Base = tile
+  c.Tool = f
+  tile = c
+
+tunnel_locations = [(x,y) for x in [0.0] for y in [0.0, 80.0 - 2.5]]
+for (i,(x,y)) in enumerate(tunnel_locations):
+  t = doc.addObject("Part::Box","Chamber{0}".format(i+1))
+  t.Width = 2.5
+  t.Length = 80.0
+  t.Height = 2.0
+  t.Placement.Base = (x,y,1.0)
+  c = doc.addObject("Part::Cut","CutT{0}".format(i+1))
+  c.Base = tile
+  c.Tool = t
+  tile = c
+
+nose_locations = [(x,y) for x in [0.0,75.0] for y in [35.0]]
+for (i,(x,y)) in enumerate(nose_locations):
+  n = doc.addObject("Part::Box","Nose{0}".format(i+1))
+  n.Width = 10.0
+  n.Length = 5.0
+  n.Height = 3.0
+  n.Placement.Base = (x,y,3.0)
+  f = doc.addObject("Part::MultiFuse","Fusion{0}".format(i))
+  f.Shapes = [n, tile]
+  tile = f
+
+d = doc.addObject("Part::Cylinder","Cylinder")
+d.Radius = 1.5
+d.Placement.Base = (40.0, 40.0, 0.0)
+c = doc.addObject("Part::Cut","CutD")
+c.Base = tile
+c.Tool = d
+tile = c
 
 doc.recompute()
+
+Gui.SendMsgToActiveView("ViewFit")
+Gui.activeDocument().activeView().viewIsometric()
--- ping-pong-display/tiles/models/top-tile.py
+++ ping-pong-display/tiles/models/top-tile.py
@@ -8,7 +8,7 @@ sphere_locations = [(x,y) for x in [20.0,60.0] for y in [20.0,60.0]]
 for (i,(x,y)) in enumerate(sphere_locations):
   s = doc.addObject("Part::Sphere","Sphere{0}".format(i+1))
   s.Radius = '19.0 mm'
-  s.Placement = App.Placement(App.Vector(x,y,16), App.Rotation(App.Vector(0,0,1),0), App.Vector(0,0,0))
+  s.Placement.Base = (x,y,16.0)
   c = doc.addObject("Part::Cut","CutS{0}".format(i+1))
   c.Base = tile
   c.Tool = s
@@ -20,10 +20,21 @@ for (i,(x,y)) in enumerate(rabbet_locations):
   r.Width = 10.0
   r.Length = 5.0
   r.Height = 3.0
-  r.Placement = App.Placement(App.Vector(x,y,0), App.Rotation(App.Vector(0,0,1),0), App.Vector(0,0,0))
+  r.Placement.Base = (x,y,0)
   c = doc.addObject("Part::Cut","CutR{0}".format(i+1))
   c.Base = tile
   c.Tool = r
   tile = c
 
+d = doc.addObject("Part::Cylinder","Cylinder")
+d.Radius = 1.5
+d.Placement.Base = (40.0, 40.0, 0.0)
+c = doc.addObject("Part::Cut","CutD")
+c.Base = tile
+c.Tool = d
+tile = c
+
 doc.recompute()
+
+Gui.SendMsgToActiveView("ViewFit")
+Gui.activeDocument().activeView().viewIsometric()
